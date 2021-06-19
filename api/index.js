7906cde763083dc2d8918c4945e74d61f8864c71
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-18 22:52:56 -0500

    Creando los modelos de la base de datos 'Order y Meal'

--- api/index.js
+++ api/index.js
@@ -14,9 +14,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-
-console.log(process.env.MONGO_URI);
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB conectado...'.green))
     .catch(err => console.log('error:', err))
