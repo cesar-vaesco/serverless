cabe3792f360e4d9c80edfdf607c5531d144c982
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-21 13:25:25 -0500

    Deshabilitando el botón de enviar al cargar meals

--- almuerzi/main.js
+++ almuerzi/main.js
@@ -6,14 +6,16 @@ window.onload = () => {
         .then(response => response.json())
         .then(data => {
             const mealsList = document.getElementById('meals-list');
+            const submit = document.getElementById('submit');
             const template = data.map(x => '<li>' + x.name + '</li>').join('')
 
             mealsList.innerHTML = template
+            submit.removeAttribute('disabled');
         })
 }
 
 /*
-    Ejemplos de cabeceras ue pueden acompañar a una petición fetch
+    Ejemplos de cabeceras que pueden acompañar a una petición fetch
     fetch('http://localhost:3000/api/meals'),{
         method: 'GET',
         mode:"cors",
