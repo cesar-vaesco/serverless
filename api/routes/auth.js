5ef657f0a00638f0dd64126a5c1645e684e49d7a
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-06-22 10:12:11 -0500

    Creando la ruta(router) de registro

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const crypto = require('crypto');
+
 const Users = require('../models/Users');
 
 const router = express.Router();
@@ -6,7 +8,26 @@ const router = express.Router();
 
 
 router.post('/register', (req, res) => {
-    res.send('Soy registro')
+    const { email, password } = req.body;
+    crypto.randomBytes(16, (err, salt) => {
+        const newSalt = salt.toString('base64');
+        crypto.pbkdf2Sync(password, newSalt, 10000, 64, 'sha1', (err, key) => {
+            const encryptedPassword = key.toString('base64');
+            Users.findOne({ email }).exec()
+                .then(user => {
+                    if (user) {
+                        return user.send('Usuario ya existe')
+                    }
+                    Users.create({
+                        email,
+                        password: encryptedPassword,
+                        salt: newSalt
+                    }).then(() => {
+                        res.send('Usuario creado con éxito!')
+                    })
+                })
+        })
+    })
 });
 
 router.post('/login', (req, res) => {
